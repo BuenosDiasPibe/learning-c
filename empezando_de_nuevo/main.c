9ada5dbbce7255d69e480c1a17f7e69d5a955895
facundo <juliabarrios782@gmail.com>
2025-11-22 23:20:28 -0300

    learning linked_lists

---
+++ empezando_de_nuevo/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+  printf("la concha de tu madre bob esponja\n");
+  return 69;
+}
---
+++ hello_wor.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+  printf("hello war!!!\n");
+  return 0;
+}
