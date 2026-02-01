1770a09081b56dca008a97d39fc120a34f9f7c8e
facundo <juliabarrios782@gmail.com>
2026-02-01 17:19:43 -0300

    finished dynamic array implementation

--- i_swear_to_god/dynamic_list.c
+++ i_swear_to_god/dynamic_list.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <assert.h>
 
-
 typedef struct {
     char name[20];
     bool finished;
@@ -19,7 +18,6 @@ void remove_end_line(char *name) {
         }
     }
 }
-
 Task create_task() {
     Task t = {};
     printf("task name: ");
@@ -34,7 +32,6 @@ typedef struct {
     size_t capacity;
 } TasksList;
 
-
 void da_append(TasksList *list, Task *item) {
     if(list->count >= list->capacity) {
         if(list->capacity == 0) list->capacity = 10;
@@ -46,7 +43,7 @@ void da_append(TasksList *list, Task *item) {
 
 void list_show(TasksList *list) {
     for(int j = 0; j < list->count; j++) {
-        printf("\nitem %s: finished: %s\n", list->items[j].name, list->items[j].finished ? "true" : "false");
+        printf("\nitem %s ; finished: %s\n", list->items[j].name, list->items[j].finished ? "true" : "false");
     }
 }
 
@@ -54,20 +51,45 @@ void task_finish(TasksList *list) {
     printf("select one from 1 to %zu: ", list->count);
     char temp[5];
     int selection = atoi(fgets(temp, sizeof(temp), stdin));
-    if(selection <= 0 || selection > (int)list->count) {printf("you selected nothing"); return;}
+    if(selection <= 0 || selection > (int)list->count) {printf("you selected nothing\n"); return;}
 
     list->items[selection-1].finished = true;
 }
 
+bool selection(TasksList *list) {
+    int select = 0;
+    char tmp[10];
+    printf("select an option:\n\t1. create a task\n\t2. show all tasks\n\t3. finish a task\n\t9. stop program\n: ");
+    select = atoi(fgets(tmp, sizeof(tmp), stdin));
+    switch(select) {
+        case 1:
+            Task t = create_task();
+            da_append(list, &t);
+            return true;
+        case 2:
+            list_show(list);
+            break;
+        case 3:
+            task_finish(list);
+            break;
+        case 9:
+            printf("goodbye!!\n");
+            return false;
+        default:
+            printf("you selected nothing...\n");
+            return true;
+    }
+    printf(": "); 
+    fgetc(stdin);
+    return true;
+}
+
 int main() {
     TasksList list = {0};
-    for(int a = 0; a < 20; a++) {
-        Task t = create_task();
-        da_append(&list, &t);
+    bool is_running = true;
+    while(is_running) {
+        is_running = selection(&list);
     }
-    task_finish(&list);
-    list_show(&list);
-
     free(list.items);
     return 0;
 }
