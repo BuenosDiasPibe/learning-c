b6ef7d97198c32d960df8ca6afb4f58b640726c4
facundo <juliabarrios782@gmail.com>
2026-03-29 10:48:39 -0300

    there's something in the screen and its not my sins

--- opengl/main.c
+++ opengl/main.c
@@ -4,6 +4,10 @@
 #include <assert.h>
 #include <stdbool.h>
 
+void framefubber_size_callback(GLFWwindow *window, int width, int height){
+    glViewport(0,0,width,height); // used for transformation between openGL coordinates and window coordinates
+}
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // max version to run
@@ -14,12 +18,26 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPACT, GL_TRUE);
 #endif
     // start window context
-    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
-    GLFWwindow  *window  = glfwCreateWindow(1000, 1000, "hi chat", monitor, NULL);
+    GLFWwindow *window = NULL;
+    if(0){ // for funsies
+        GLFWmonitor *monitor = glfwGetPrimaryMonitor(); // apparently is for fullscren?
+        window  = glfwCreateWindow(100, 100, "hi chat", monitor, NULL);
+    } else {
+        window = glfwCreateWindow(100, 100, "hi chat", NULL, NULL);
+    }
     assert(window != NULL); // window shall be created
     glfwMakeContextCurrent(window);
 
     assert(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)); // glad shall work
+    glfwSetFramebufferSizeCallback(window, framefubber_size_callback); // set callback to resize window
 
+    while(!glfwWindowShouldClose(window)){
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+            glfwSetWindowShouldClose(window, true);
+        }
+        glfwSwapBuffers(window); // for double buffering
+        glfwPollEvents(); // for getting events (eg: windowClose)
+    }
+    glfwTerminate(); // finishes the glfw library
     return 0;
 }
